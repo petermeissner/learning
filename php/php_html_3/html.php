0c01aa08c58fd78205baef2bdd30e3ab06b95b97
petermeissner <retep.meissner@gmail.com>
2017-04-06 23:49:00 +0200

    bootstrap works

--- php/php_html_3/html.php
+++ php/php_html_3/html.php
@@ -42,13 +42,17 @@ class tag
 }
 
 
-function newtag($name="", $content = array(), $attributes = array(), $close = true){
-  $tag = new tag($name, $content, $atributes, $close);
-  return $tag;
-}
 
-class HTML
+class htmlpage
     {
+        // template settings
+         protected $head_template_pre  = array();
+         protected $head_template_post = array();
+         protected $body_template_pre  = array();
+         protected $body_template_post = array();
+
+
+        // setting character encoding of page
         public $encoding;
     
         // base structure of head
@@ -63,7 +67,7 @@ class HTML
         }
         
         // add content to head, body, ...
-        private function add($what, $where="body") {
+        protected function add($what, $where="body") {
             if( $where === "body" ){
               array_push($this->body["content"], $what);
             }else if ( $where === "head" ){
@@ -97,17 +101,22 @@ class HTML
                 "<!DOCTYPE HTML>",
                 "<html>",
                 "<head>",
+                  encoding => $encoding,
+                  head_template_pre => $this->head_template_pre,
                   content => array(),
+                  head_template_post => $this->head_template_post,
                 "</head>"
               );
 
-            $this->head['content']['encoding'] = $encoding;
+            // $this->head['content']['encoding'] = $encoding;
 
             // buidling up body
             $this->body = 
               array(
                 "<body>",
+                  body_template_pre => $this->body_template_pre,
                   content => array(),
+                  body_template_post => $this->body_template_post,
                 "</body>",
                 "</html>"
               );
@@ -118,5 +127,118 @@ class HTML
         }
     }
 
-// TODO: links zu scripten anpassen!!!
+class bootstrap_htmlpage extends htmlpage 
+{
+    // template settings
+    protected $head_template_pre  = 
+      array(
+        '<meta http-equiv="X-UA-Compatible" content="IE=edge">',
+        '<meta name="viewport" content="width=device-width, initial-scale=1">',
+        '<meta name="description" content="">',
+        '<meta name="author" content="">',
+        '<link rel="icon" href="../../favicon.ico">',
+        '<title>Dashboard Template for Bootstrap</title>',
+        '<link href="/css/bootstrap.min.css" rel="stylesheet">',
+        '<link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">',
+        '<link href="dashboard.css" rel="stylesheet">',
+        '<script src="../../assets/js/ie-emulation-modes-warning.js"></script>',
+      );
+
+    protected $head_template_post = 
+      array(
+
+      );
+
+    protected $body_template_pre  = 
+      array(
+        '
+            <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Project name</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Dashboard</a></li>
+            <li><a href="#">Settings</a></li>
+            <li><a href="#">Profile</a></li>
+            <li><a href="#">Help</a></li>
+          </ul>
+          <form class="navbar-form navbar-right">
+            <input type="text" class="form-control" placeholder="Search...">
+          </form>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-3 col-md-2 sidebar">
+          <ul class="nav nav-sidebar">
+            <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
+            <li><a href="#">Reports</a></li>
+            <li><a href="#">Analytics</a></li>
+            <li><a href="#">Export</a></li>
+          </ul>
+          <ul class="nav nav-sidebar">
+            <li><a href="">Nav item</a></li>
+            <li><a href="">Nav item again</a></li>
+            <li><a href="">One more nav</a></li>
+            <li><a href="">Another nav item</a></li>
+            <li><a href="">More navigation</a></li>
+          </ul>
+          <ul class="nav nav-sidebar">
+            <li><a href="">Nav item again</a></li>
+            <li><a href="">One more nav</a></li>
+            <li><a href="">Another nav item</a></li>
+          </ul>
+        </div>
+        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+          <h1 class="page-header">Dashboard</h1>
+
+          <div class="row placeholders">
+            <div class="col-xs-6 col-sm-3 placeholder">
+              <img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">
+              <h4>Label</h4>
+              <span class="text-muted">Something else</span>
+            </div>
+            <div class="col-xs-6 col-sm-3 placeholder">
+              <img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">
+              <h4>Label</h4>
+              <span class="text-muted">Something else</span>
+            </div>
+            <div class="col-xs-6 col-sm-3 placeholder">
+              <img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">
+              <h4>Label</h4>
+              <span class="text-muted">Something else</span>
+            </div>
+            <div class="col-xs-6 col-sm-3 placeholder">
+              <img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">
+              <h4>Label</h4>
+              <span class="text-muted">Something else</span>
+            </div>
+          </div>
+
+          <h2 class="sub-header">Section title</h2>
+        '
+      );
+
+    protected $body_template_post = 
+      array(
+        '
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+          <script>window.jQuery || document.write(\'<script src="../../assets/js/vendor/jquery.min.js"><\/script>\')</script>
+          <script src="../../dist/js/bootstrap.min.js"></script>
+          <script src="../../assets/js/vendor/holder.min.js"></script>
+          <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+        '
+      );
+}
+
 ?>
--- php/php_html_3/index.php
+++ php/php_html_3/index.php
@@ -2,10 +2,9 @@
 include_once 'tools.php';
 include_once 'html.php';
 
-$html = new HTML();
-$tag = new tag($name = "p", "Wohoooo!", $attributes = array(style => "font-size: 150%;"));
-$html->add_to_body($tag);
-$html->put();
+$htmlpage = new bootstrap_htmlpage();
+$htmlpage->add_to_body("ola");
+$htmlpage->put();
 
 
 
