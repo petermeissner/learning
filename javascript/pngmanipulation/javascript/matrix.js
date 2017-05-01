1c367b088c353be55937ae6630d76d9b6b2b1448
petermeissner <retep.meissner@gmail.com>
2017-05-01 21:52:43 +0200

    matrix work

--- javascript/pngmanipulation/javascript/matrix.js
+++ javascript/pngmanipulation/javascript/matrix.js
@@ -7,10 +7,13 @@
 
                     // create matrix object 
                     let matrix = {
-                        data:    array,
-                        columns: columns,
-                        rows:    rows,
-                        get:     function(row, column){
+                        "data":    array,
+
+                        "columns": columns,
+
+                        "rows":    rows,
+
+                        "get":     function(row = null, column = null){
                             // check inputs
                             if(row > this.rows || row < 1){
                                 throw "matrix.get(): row requested out of range";
@@ -18,9 +21,9 @@
                             if(column > this.columns || column < 1 ){
                                 throw "matrix.get(): column requested out of range";
                             }
-                            
+
                             // calculate index 
-                            let index = ( (row-1) * this.rows ) + column - 1; 
+                            let index = ( (row-1) * this.columns ) + column - 1; 
                             
                             // retrieve data from 
                             let res   = this.data[index];
@@ -28,28 +31,8 @@
                             // return
                             return res;
                           },
-                        set:     function(row, column, value){
-                            // check inputs
-                            if(row > this.rows || row < 1){
-                                throw "matrix.get(): row requested out of range";
-                            }
-                            if(column > this.columns || column < 1 ){
-                                throw "matrix.get(): column requested out of range";
-                            }
-                            
-                            // calculate index 
-                            let index = ( (row-1) * this.rows ) + column - 1; 
-                            
-                            // replace data at index 
-                            let res    = [];
-                                res[0] = this.data[index];
-                                res[1] = value;
-                            this.data[index] = value;
-                            
-                            // return
-                            return res;
-                        },
-                        as_tableBody: function(){
+
+                        "as_tableBody": function(){
                             // build tableBody 
                             let tableBody = document.createElement('tbody');
                             for (let row = 1; row <= this.rows; row++) {
@@ -64,9 +47,34 @@
                                     line.appendChild(cell);
                                 }
                                 tableBody.appendChild(line);
+                                tableBody.style.fontFamily = "monospace"
                             }
                             // return
                             return tableBody; 
+                        },
+
+                        "random_get": function(n = 1){
+                            let res = []
+                            let idx = 0;
+                            let col = 0;
+                            let rw  = 0;
+
+                            for (var index = 0; index < n; index++) {
+                                let idx = Math.floor( Math.random() * this.data.length );
+                                let col = idx % this.rows + 1;
+                                let rw  = idx % this.columns + 1;
+                                res.push(
+                                    {
+                                        index:  idx,
+                                        column: col, 
+                                        row:    rw, 
+                                        value:  this.data[idx]
+                                    }
+                                );
+                            }
+;
+                            // return 
+                            return res; 
                         }
                     };
 
