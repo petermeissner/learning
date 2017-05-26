b749595fa8084cad3c7c24594f050549c4244a27
petermeissner <retep.meissner@gmail.com>
2017-05-26 23:43:21 +0200

    make phpcs happy

--- php/api/api.php
+++ php/api/api.php
@@ -1,21 +1,23 @@
-<?php 
-
+<?php
 echo $_REQUEST["path"];
-if( basename($_REQUEST["path"]) === "info" ){
-    // produce filename
-    $info_file = dirname($_REQUEST["path"]) . "/info.php"; 
-    // use file name or fall back to global info
-    if( !file_exists($info_file) ){
-      $info_file; 'info.php';
-    }
-    $infopath = "api/" . $info_file;
-    include $info_file ;
-    exit;
-}
 
 echo "<pre>SERVER\n";
+
 var_dump($_REQUEST);
+
 echo "\n-------</pre>";
 
 echo "http://coreymaynard.com/blog/creating-a-restful-api-with-php/";
 
+// fallback option -> show info 
+
+  // produce filename
+  $info_file = dirname($_REQUEST["path"]) . "/info.php";
+  
+  // use file name or fall back to global info
+if (!file_exists($info_file)) {
+    $info_file = 'info.php';
+}
+  $infopath = "api/" . $info_file;
+    include $info_file ;
+    exit;
